1b8da7446ffa5809ebc4b09a2c072439372e85de
unknown <iiigogor@gmail.com>
2021-12-02 21:06:13 +0300

    init

--- main.java
+++ main.java
@@ -1,13 +1,16 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
-int chek=100;
-        int make=1001;
+        int chek = 100;
+        int make = 999;
         int bonus;
         int total;
-        if (make <=1000) {bonus=0;
+        if (make <= 1000) {
+            bonus = 0;
             System.out.println("Бонусов нет");
-        }else{bonus=make/100;}
-        total=chek+make+bonus;
-        System.out.println("На Вашем счете "+total+"рублей");
+        } else {
+            bonus = make / 100;
+        }
+        total = chek + make + bonus;
+        System.out.println("На Вашем счете " + total + "рублей");
     }
 }
